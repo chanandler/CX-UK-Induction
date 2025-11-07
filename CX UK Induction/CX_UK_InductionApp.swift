2dd4c91c59be3008349897825edc7c32827f65f3
Clint Yarwood <chanandler@me.com>
2025-11-07 21:06:02 +0000

    Initial Commit

---
+++ CX UK Induction/CX_UK_InductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CX_UK_InductionApp.swift
+//  CX UK Induction
+//
+//  Created by Clint Yarwood on 07/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CX_UK_InductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
