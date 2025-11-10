ecb1ccae1c84087d79951fb85bdf4d84e801884c
Clint Yarwood <chanandler@me.com>
2025-11-10 21:59:59 +0000

    More updates

--- CX UK Induction/RootView.swift
+++ CX UK Induction/RootView.swift
@@ -4,19 +4,21 @@ import SwiftData
 struct RootView: View {
     var body: some View {
         ZStack {
-            Color.white
+            Color(red: 2/255, green: 49/255, blue: 133/255)
                 .ignoresSafeArea()
 
-            VStack(alignment: .center, spacing: 12) {
-                Text("Welcome to Cemex UK HQ")
-                    .font(.system(size: 48, weight: .bold))
-                    .multilineTextAlignment(.center)
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .padding(.top, 8)
-
-                WelcomeView()
+            GeometryReader { geo in
+                VStack(alignment: .center, spacing: 12) {
+                    WelcomeView()
+                }
+                .padding(.horizontal)
+                .frame(maxWidth: 540)
+                .background(Color.white)
+                .cornerRadius(12)
+                .shadow(color: Color.black.opacity(0.1), radius: 8, x: 0, y: 4)
+                .frame(width: geo.size.width, height: geo.size.height)
+                .position(x: geo.size.width / 2, y: geo.size.height / 2)
             }
-            .padding(.horizontal)
         }
     }
 }
